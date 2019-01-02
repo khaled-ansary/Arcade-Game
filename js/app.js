00c4176f39a290fefff6009546ab63ecb6e07bf6
Khaled Hossain Ansary <ansary.development@gmail.com>
2019-01-02 01:17:22 +0100

    - set timer - set win logic and score - set messages to play - implement enter key operation

--- js/app.js
+++ js/app.js
@@ -37,19 +37,36 @@ class Enemy{
     
     // Draw the enemy on the screen, required method for game
     render() {
+
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.fillStyle = "white";
+        ctx.font = "16px Comic Sans MS";
+        ctx.fillText(`Score: ${score}`, 100, 70);
+        ctx.fillText(`time: ${timeModel.min} mins ${timeModel.sec} sec` , 200, 70);
+
+        // if score reach the win score, it will give a message and 
+        // hold the game
+        // we can restart the game by press 'enter'
+        if (score >= MAX_SCORE) {
+            ctx.fillStyle = "green";
+            ctx.font = "20px Comic Sans MS";
+            ctx.fillText(`You win the Game Score: ${score}`, 141, 200);
+            ctx.fillText(`time takes: ${timeModel.min} mins ${timeModel.sec} sec` , 141, 225);
+            canvas.clearTimer();
+            timeModel.isStop = true;   
+        }
     }
 
     // checks en enemy's collision with player
     checkCollision() {
         var playerBox = {x: player.x, y: player.y, width: 50, height: 40};
         var enemyBox = {x: this.x, y: this.y, width: 60, height: 70};
-        // Check for collisions, if playerBox intersects enemyBox, we have one
+        // Check for collisions, if playerBox intersects enemyBox, score decrease by 1
+        // and the setset the player and enemies
         if (playerBox.x < enemyBox.x + enemyBox.width &&
             playerBox.x + playerBox.width > enemyBox.x &&
             playerBox.y < enemyBox.y + enemyBox.height &&
             playerBox.height + playerBox.y > enemyBox.y)  {
-            console.log('collision');
             this.resetGame();
             canvas.decreaseScore();
         }
@@ -80,7 +97,7 @@ class Player{
         this.speed = speed;
         this.sprite = 'images/char-boy.png';
 
-        this.wall = {left: 101, right: 402, top: 0, bottom: 383};
+        this.wall = {left: 101, right: 402, top: 0, bottom: 383, middle: 202.5};
         this.move = {x: 101, y: 83};
     }
 
@@ -92,6 +109,13 @@ class Player{
     // renders the player
     render() {        
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        if(timeModel.isStop)
+        {
+            ctx.fillStyle = "blue";
+            ctx.font = "16px Comic Sans MS";
+            ctx.fillText(`press "enter" to restart the Game` , 141, 255);
+            ctx.fillText(`use arrow keys to move ` , 141, 275);
+        }
     }
 
     /*
@@ -102,32 +126,43 @@ class Player{
         switch (key) {
             case "left":
                 //check for wall, otherwise move left
-                if (this.x > this.wall.left) {
+                if (this.x > this.wall.left && !timeModel.isStop) {
                     this.x -= this.move.x;
                 }
                 break;
             case "right":
                 //check for wall, otherwise move right
-                if (this.x < this.wall.right) {
+                if (this.x < this.wall.right && !timeModel.isStop) {
                     this.x += this.move.x;
                 }
                 break;
             case "up":
                 //check if player reached top of water, if so call success function,
                 // otherwise move up
-                if (this.y < this.wall.top) {
+                if (this.y < this.wall.top && !timeModel.isStop) {
                     canvas.inceaseScore();
                     canvas.completeLevel();
-                } else {
+                } else if(!timeModel.isStop) {
                     this.y -= this.move.y;
                 }
                 break;
             case "down":
                 //check for bottom, otherwise move down
-                if (this.y < this.wall.bottom) {
+                if (this.y < this.wall.bottom && !timeModel.isStop) {
                     this.y += this.move.y;
                 }
                 break;
+            
+            case "start":
+                // enter to start/restart the game   
+                if (timeModel.isStop) {
+                    addDiv.innerHTML = '';             
+                    timeModel.isStop = false;
+                    canvas.setTimer();
+                    canvas.completeLevel();
+                    score = 0;
+                    break;    
+                }                
         }
     }
         
@@ -135,15 +170,14 @@ class Player{
     * resets the player to default position
     */
     reset(){
-        this.x = 202.5;
+        this.x = this.wall.middle;
         this.y = this.wall.bottom;
     }
 }
 
 
-
 class Canvas {
-    
+
     initListener(){
         // This listens for key presses and sends the keys to your
         // Player.handleInput() method. You don't need to modify this.
@@ -152,18 +186,12 @@ class Canvas {
                 37: 'left',
                 38: 'up',
                 39: 'right',
-                40: 'down'
+                40: 'down',
+                13: 'start'
             };
             player.handleInput(allowedKeys[e.keyCode]);
         });
 
-    }
-    
-    /*
-    * updates the on screen score display
-    */
-    updateDisplay() {
-        scoreDiv.innerHTML = 'Your Score ' + score;
     }
 
     /**
@@ -171,7 +199,6 @@ class Canvas {
      */
     inceaseScore() {
         score += 1;
-        this.updateDisplay();
     }
 
     /**
@@ -182,9 +209,8 @@ class Canvas {
         if(score < 1){
             score = 0;
         }
-        this.updateDisplay();
-    } 
-
+    }
+    
     /*
     * complete level successfully (reached water)
     */
@@ -194,24 +220,54 @@ class Canvas {
             allEnemies.push(new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70));
         }
     }
+
+    /**
+     * set timer 
+     * */ 
+    setTimer(){
+        
+        interval = setInterval(function() {
+            timeModel.sec++;
+            if (timeModel.sec === 60) {
+                timeModel.min++;
+                timeModel.sec = 0;
+            }
+            if (timeModel.min >= 60) {
+                timeModel.hr++;
+                timeModel.min = 0;
+            }
+        }, 1000);
+
+        return interval;
+    }
     
+    /**
+     * clear timer
+     */
+    clearTimer(){
+        clearInterval(interval);
+    }
+
 }
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let allEnemies = [];
+let timeModel = {min: 0, sec: 0, hr: 0, isStop: true};
+let interval;
 const player = new Player(0, 0, 50);
+player.reset();
 
 canvasDiv = document.getElementsByTagName('canvas')[0];
-scoreDiv = document.createElement('div');
-document.body.insertBefore(scoreDiv, canvasDiv);
+addDiv = document.createElement('div');
+document.body.insertBefore(addDiv, canvasDiv);
 // set score
 let score = 0;
-const canvas = new Canvas(canvasDiv, scoreDiv, player, score);
+const MAX_SCORE = 10;
+const canvas = new Canvas();
 canvas.initListener();
-canvas.updateDisplay();
-canvas.completeLevel();
+
 
 
 
